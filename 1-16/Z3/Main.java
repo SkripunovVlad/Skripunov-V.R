0b46cd914825de2ee142b0509ba8cf346594ccdc
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-10 18:41:38 +0300

    Main.java

--- 1-16/Z3/Main.java
+++ 1-16/Z3/Main.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        float M[][] = new float[3][2];
+        int tochka = 1;
+        for(int i = 0; i < 3; i++){
+            System.out.println( tochka + "я точка: ");
+            tochka++;
+            for(int j = 0; j < 2; j++){
+                M[i][j] = in.nextInt();
+            }
+        }
+        System.out.println("{" + (M[0][0] + M[1][0] + M[2][0])/3 + "," + (M[0][1] + M[1][1] + M[2][1])/3 + "}");
+    }
+}
