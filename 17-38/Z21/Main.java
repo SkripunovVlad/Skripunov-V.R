341eeed04b727d4ab0f382b7c5e9b7764e682548
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-12 14:15:35 +0300

    Update Main.java

--- 17-38/Z21/Main.java
+++ 17-38/Z21/Main.java
@@ -1,1 +1,25 @@
-
+package Main;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner gg = new Scanner(System.in);
+        String m = gg.nextLine();
+        String[] as = m.split("");
+        String gl = "";
+        for(int i = 0; i < as.length; i++){
+            int pl = Integer.parseInt(as[i]);
+            if(as[i].equals("1")||as[i].equals("2")||as[i].equals("3")) {
+                pl += 6;
+                gl += pl;
+            }
+            if(as[i].equals("4")||as[i].equals("5")||as[i].equals("6")||as[i].equals("0")){
+                gl += pl;
+            }
+            if(as[i].equals("7")||as[i].equals("8")||as[i].equals("9")){
+                pl -= 6;
+                gl += pl;
+            }
+        }
+        System.out.println(gl);
+    }
+}
