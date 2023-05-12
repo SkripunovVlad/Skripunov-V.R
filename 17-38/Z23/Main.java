69bc54cd27f1fbd41d86ea89f5b9558e2da036e0
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-12 14:26:41 +0300

    Update Main.java

--- 17-38/Z23/Main.java
+++ 17-38/Z23/Main.java
@@ -1,1 +1,23 @@
+package Main;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число N: ");
+        int n = input.nextInt();
 
+        System.out.print("Введите первый делитель: ");
+        int d1 = input.nextInt();
+
+        System.out.print("Введите второй делитель: ");
+        int d2 = input.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % d1 == 0 || i % d2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("Сумма чисел, делящихся на " + d1 + " или " + d2 + ": " + sum);
+    }
+}
