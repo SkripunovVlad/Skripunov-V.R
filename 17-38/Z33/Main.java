38550b712cb867bcf9dd84f5469901d1dda18a23
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-12 15:34:27 +0300

    Update Main.java

--- 17-38/Z33/Main.java
+++ 17-38/Z33/Main.java
@@ -1,1 +1,26 @@
+package Main;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long tanyaSticks = 0; 
+        while (n > 0) {
+            
+            if (n % 2 == 1) {
+                n--;
+                tanyaSticks++;
+            } else {
+                long tanyaHalf = n / 2;
+                long tanyaOne = n - 1;
+                if (tanyaHalf > tanyaOne) {
+                    n /= 2;
+                } else {
+                    n--;
+                }
+            }
+        }
+        System.out.println(tanyaSticks);
+    }
+}
 
