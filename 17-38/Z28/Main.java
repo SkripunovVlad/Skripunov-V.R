479488c298e343c07d4270c6a3c9e81b8a202212
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-12 14:56:58 +0300

    Update Main.java

--- 17-38/Z28/Main.java
+++ 17-38/Z28/Main.java
@@ -1,1 +1,17 @@
-
+package Main;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int i = 1;
+        while (i * i < n) {
+            i++;
+        }
+        int closestSquare = i * i;
+        int diff = closestSquare - n;
+        int root = (int) Math.sqrt(diff);
+        int result = root * 2 + 1;
+        System.out.println(result);
+    }
+}
