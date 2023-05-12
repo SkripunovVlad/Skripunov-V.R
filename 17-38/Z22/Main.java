7e5495ac459c556caea796edcadd2b70405d5c48
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-12 14:22:03 +0300

    Update Main.java

--- 17-38/Z22/Main.java
+++ 17-38/Z22/Main.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длины сторон треугольника:");
+        System.out.print("сторона a = ");
+        double a = scanner.nextDouble();
+        System.out.print("сторона b = ");
+        double b = scanner.nextDouble();
+        System.out.print("сторона c = ");
+        double c = scanner.nextDouble();
+        double area = calculateTriangleArea(a, b, c);
+        System.out.println("Площадь треугольника: " + area);
+    }
+
+    public static double calculateTriangleArea(double a, double b, double c) {
+        double s = (a + b + c) / 2;
+        double area = Math.sqrt(s * (s - a) * (s - b) * (s - c)); 
+        return area;
+    }
+
+}
