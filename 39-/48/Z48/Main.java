6bfe175fac912482b7f79113470e2ff964dd8689
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-17 23:02:38 +0300

    Update Main.java

--- 39-/48/Z48/Main.java
+++ 39-/48/Z48/Main.java
@@ -1,15 +1,24 @@
 import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
         int x = 10;
-        HashSet<Integer> u = new HashSet<Integer>(); 
+        HashSet<Integer> u = new HashSet<Integer>();
         for (int i = 0; i <= x; i++) {
             int y = 2 * i + 1;
             int z = 3 * i + 1;
-            u.add(y);
-            u.add(z);
+            if (!u.contains(y)) {
+                u.add(y);
+            }
+            if (!u.contains(z)) {
+                u.add(z);
+            }
+            for (int j = y + 1; j < z; j++) {
+                if (!u.contains(j)) {
+                    u.add(j);
+                }
+            }
         }
-        System.out.println("Уникальные значения ряда u:");
         for (int num : u) {
             System.out.print(num + " ");
         }
