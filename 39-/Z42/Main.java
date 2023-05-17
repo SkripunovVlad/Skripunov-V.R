3561a0e0ed61f0ff7a6bb2f62e17f568dcd98a11
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-17 21:33:04 +0300

    Update Main.java

--- 39-/Z42/Main.java
+++ 39-/Z42/Main.java
@@ -1,1 +1,24 @@
-
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 2}, {6, 10}, {11, 15}};
+        int count = countNumbersBetweenIntervals(intervals);
+        System.out.println("Количество чисел между интервалами: " + count);
+    }
+    public static int countNumbersBetweenIntervals(int[][] intervals) {
+        int[] sorted = new int[intervals.length * 2];
+        int index = 0;
+        for (int[] interval : intervals) {
+            sorted[index++] = interval[0];
+            sorted[index++] = interval[1];
+        }
+        Arrays.sort(sorted);
+        int count = 0;
+        for (int i = 0; i < sorted.length - 1; i++) {
+            if (sorted[i] != sorted[i + 1]) {
+                count += sorted[i + 1] - sorted[i] - 1;
+            }
+        }
+        return count;
+    }
+}
