306084fc9a17ec204de7607abce71afa7ebb4855
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-05-29 19:50:31 +0300

    Update Main.java

--- Гавшин/Z3/Main.java
+++ Гавшин/Z3/Main.java
@@ -6,17 +6,19 @@ public class Main {
         list.add(1, "Элемент 1");
         list.add(2, "Элемент 2");
         list.add(3, "Элемент 3");
+        list.add(4, "Элемент 4");
+        list.add(5, "Элемент 5");
+        list.add(6, "Элемент 6");
 
         // Поиск элемента по ключу
-        Object element = list.search(2);
-        System.out.println("Элемент с ключом 2: " + element);
+        Object element = list.search(5);
+        System.out.println("Элемент с ключом 5: " + element);
 
         // Удаление элемента по ключу
-        list.remove(2);
+        list.remove(5);
 
         // Проверка, что элемент удален
-        element = list.search(2);
-        System.out.println("Элемент с ключом 2 (после удаления): " + element);
+        element = list.search(5);
+        System.out.println("Элемент с ключом 5 (после удаления): " + element);
     }
 }
-
