b279242d6d122a25f2a15e6472f14f4456e83543
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-06-01 08:38:51 +0300

    Update SinglyLinkedList.java

--- Гавшин/Z3/SinglyLinkedList.java
+++ Гавшин/Z3/SinglyLinkedList.java
@@ -5,30 +5,39 @@ public class SinglyLinkedList {
         this.head = null;
     }
 
+    // Метод для добавления элемента в конец списка
     public void add(int key) {
         Node newNode = new Node(key);
         if (head == null) {
+            // Если список пуст, новый элемент становится головой списка
             head = newNode;
         } else {
             Node current = head;
+            // Находим последний элемент списка
             while (current.getNext() != null) {
                 current = current.getNext();
             }
+            // Присоединяем новый элемент к последнему элементу
             current.setNext(newNode);
         }
     }
 
+    // Метод для удаления элемента из списка
     public void remove(int key) {
         if (head == null) {
+            // Если список пуст, ничего не делаем
             return;
         }
 
         if (head.getKey() == key) {
+            // Если элемент для удаления - голова списка, переустанавливаем голову
             head = head.getNext();
         } else {
             Node current = head;
+            // Ищем элемент для удаления
             while (current.getNext() != null) {
                 if (current.getNext().getKey() == key) {
+                    // Если найден элемент, перескакиваем через него, удаляя его из списка
                     current.setNext(current.getNext().getNext());
                     return;
                 }
@@ -37,17 +46,22 @@ public class SinglyLinkedList {
         }
     }
 
+    // Метод для поиска элемента в списке
     public Node search(int key) {
         Node current = head;
+        // Ищем элемент с заданным ключом
         while (current != null) {
             if (current.getKey() == key) {
+                // Если найден элемент, возвращаем его
                 return current;
             }
             current = current.getNext();
         }
+        // Если элемент не найден, возвращаем null
         return null;
     }
 
+    // Метод для вывода элементов списка на экран
     public void display() {
         Node current = head;
         while (current != null) {
@@ -60,21 +74,24 @@ public class SinglyLinkedList {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
 
+        // Добавляем элементы в список
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
 
-        list.display(); // Output: 1 2 3 4
+        list.display(); // Вывод: 1 2 3 4
 
+        // Удаляем элемент из списка
         list.remove(3);
-        list.display(); // Output: 1 2 4
+        list.display(); // Вывод: 1 2 4
 
+        // Ищем элемент в списке
         Node foundNode = list.search(2);
         if (foundNode != null) {
-            System.out.println("Found node with key 2");
+            System.out.println("Найден элемент с ключом 2");
         } else {
-            System.out.println("Node with key 2 not found");
+            System.out.println("Элемент с ключом 2 не найден");
         }
     }
 }
