1129b4c9ef5025abe25b33e415e66931ddcd1983
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-06-01 08:39:53 +0300

    Update Node.java

--- Гавшин/Z3/Node.java
+++ Гавшин/Z3/Node.java
@@ -1,22 +1,21 @@
 public class Node {
-    private int key;
-    private Node next;
+    private int key; // Ключ узла
+    private Node next; // Ссылка на следующий узел
 
     public Node(int key) {
-        this.key = key;
-        this.next = null;
+        this.key = key; // Инициализация ключа узла
+        this.next = null; // Инициализация ссылки на следующий узел как null
     }
 
     public int getKey() {
-        return key;
+        return key; // Возвращает ключ узла
     }
 
     public Node getNext() {
-        return next;
+        return next; // Возвращает ссылку на следующий узел
     }
 
     public void setNext(Node next) {
-        this.next = next;
+        this.next = next; // Устанавливает ссылку на следующий узел
     }
 }
-
