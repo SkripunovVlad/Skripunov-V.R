c5a7f6dd8dfde725786502a859eeacaa41e87e91
SkripunovVlad <113343293+SkripunovVlad@users.noreply.github.com>
2023-12-12 19:21:55 +0300

    Delete 1-16 directory

--- 1-16/Z7/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-        Scanner n = new Scanner(System.in);
-        int num = n.nextInt();
-        System.out.println(Math.round(Math.sqrt(num)));
-    }
-}
